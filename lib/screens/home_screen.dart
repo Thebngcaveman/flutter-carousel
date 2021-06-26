4a4c556139904dcb2a0330bfed28f6ee7bc6f6cd
suppanut.sittimakarn <suppanut.sittimakarn@accenture.com>
2021-06-26 22:23:48 +0700

    add photos and update yml file and initialize photos list

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int photoIndex = 0;
+
+  List<String> photos =[
+    'assets/images/castle.png',
+    'assets/images/desert.png',
+    'assets/images/egypt.png',
+    'assets/images/snow.png'
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Carousel'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+    );
   }
 }
